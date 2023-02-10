692f5911767e39de85716c18eb8b390ed60bc675
Alice PS <alisastern1@gmail.com>
2023-02-11 01:00:10 +0300

    123

--- src/test/scala/perf/Actions.scala
+++ src/test/scala/perf/Actions.scala
@@ -1,6 +1,7 @@
 package perf
 
 import io.gatling.core.Predef._
+import io.gatling.core.check.extractor.regex.{RegexCheckBuilder, RegexOfType}
 import io.gatling.http.Predef._
 
 object Actions {
@@ -58,21 +59,22 @@ object Actions {
     .check(status is 200)
     .check(regex("""name="departDate" value="(\d{2}\/\d{2}\/\d{4})"""").saveAs("departDate"))
 
+  val date = "(d{2}/d{2}/d{4})".r
 
   val ReservationsPl = http(requestName = "/cgi-bin/reservations.pl/")
     .post("/cgi-bin/reservations.pl/")
     .formParam("advanceDiscount", "0")
     .formParam("depart","${depart}")
-    .formParam("departDate", "${departDate}")
+    .formParam("departDate", "$(date)")
     .formParam("arrive","${depart}")
-    .formParam("returnDate", "${departDate}")
+    .formParam("returnDate", "$(date)")
     .formParam("numPassengers", "${numPassengers}")
     .formParam("seatPref", "${seatPref}")
     .formParam("seatType","${seatType}")
     .formParam("findFlights.x","37")
     .formParam("findFlights.y","12")
     .check(status is 200)
-    .check(regex("""name="outboundFlight" value="(.+)"""").saveAs("outboundFlight"))
+    .check(regex("""name="outboundFlight" value="(\d{3}.;\d{3}.;\d{2}\./d{2}\./d{4})"""").saveAs("outboundFlight"))
 
 val ReservationsPl2 = http(requestName = "/cgi-bin/reservations.pl/")
 .post("/cgi-bin/reservations.pl/")
@@ -100,24 +102,23 @@ val ReservationsPl2 = http(requestName = "/cgi-bin/reservations.pl/")
 
 val ReservationsPl4 = http(requestName = "/cgi-bin/reservations.pl/")
 .post("/cgi-bin/reservations.pl/")
-.formParam("firstName", "${outboundFlight}")
-.formParam("lastName", "${numPassengers}")
-.formParam("address1", "0")
-.formParam("address2","${seatType}")
-.formParam("pass1", "${seatPref}")
-.formParam("creditCard","42")
-.formParam("expDate","8")
-  .formParam("oldCCOption", "nil")
+.formParam("firstName", "Hungry")
+.formParam("lastName", "Always")
+.formParam("address1", "London")
+.formParam("address2","Baker street")
+.formParam("pass1", "Always Hungry")
+.formParam("creditCard","1111111111111111")
+.formParam("expDate","08/25")
+  .formParam("oldCCOption", "on")
     .formParam("numPassengers", "${numPassengers}")
     .formParam("advanceDiscount", "0")
     .formParam("seatType","${seatType}")
     .formParam("seatPref", "${seatPref}")
   .formParam("outboundFlight", "${outboundFlight}")
-  .formParam("advanceDiscount", "${outboundFlight}")
-  .formParam("returnFlight", "${outboundFlight}")
-  .formParam("JSFormSubmit", "${outboundFlight}")
-    .formParam("buyFlights.x","42")
-    .formParam("buyFlights.y","8")
+  .formParam("returnFlight", null)
+  .formParam("JSFormSubmit", "off")
+    .formParam("buyFlights.x","40")
+    .formParam("buyFlights.y","11")
 .check(status is 200)
 
   val Itinerary1 = http(requestName = "/cgi-bin/welcome.pl")
--- src/test/scala/perf/TestSimulation.scala
+++ src/test/scala/perf/TestSimulation.scala
@@ -14,16 +14,21 @@ class TestSimulation extends Simulation {
     .acceptLanguageHeader("en-US,en;q=0.5")
     .disableFollowRedirect
 
-  setUp(
-    CommonScenario().inject(
+  <!-- setUp(
+    CommonScenario().inject(atOnceUsers(users = 1)
 
-      incrementUsersPerSec(6)
+     incrementUsersPerSec(6)
         .times(10)
         .eachLevelLasting(60)
         .separatedByRampsLasting(60)
         .startingFrom(0)
+
     )
   ).protocols(httpProtocol)
+}-->
+setUp(
+CommonScenario().inject(atOnceUsers(users = 2)
+)
+).protocols(httpProtocol)
 }
-
 
